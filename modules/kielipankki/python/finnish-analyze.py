ef0335290fba2593266c664043dca0951fbcba06
Martin Matthiesen <martin.matthiesen@csc.fi>
2016-03-02 11:47:31 +0200

    moved demo modules

---
+++ modules/kielipankki/python/finnish-analyze.py
@@ -0,0 +1,13 @@
+# TOOL finnish-analyze.py: "finnish-analyze.sh on Taito" 
+# INPUT input TYPE GENERIC
+# OUTPUT output
+
+
+import subprocess
+import os
+
+my_cmd='iconv -f cp1252 -t utf-8 input|finnish-analyze.sh |iconv -futf-8 -tcp1252 > output'
+
+p = subprocess.Popen(my_cmd, shell=True)
+os.waitpid(p.pid, 0)
+
