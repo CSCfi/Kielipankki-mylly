048b1f303658ac6cfc4b421f5b864f9abb81ceb6
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2016-03-04 16:06:54 +0200

    Producing token file as both tokens.txt and tokens.tsv
    Naming is not cosmetic! Kielipankki Chipster offers to show *.txt
    right as text, *.tsv as spreadsheet, and the latter FAILED with
    exception (reported using the tiny message box in Chipster). External
    browser offered and managed to open *.tsv in Calc, so that naming is
    also useful.

--- modules/kielipankki/python3/demotokens.py
+++ modules/kielipankki/python3/demotokens.py
@@ -1,8 +1,14 @@
 # TOOL demotokens.py: "Simply Tokenize Plain Text" (Writes one token per line, with line and token number)
 # INPUT text.txt TYPE GENERIC
+# OUTPUT tokens.txt (Token per line, with line and token number)
 # OUTPUT tokens.tsv (Token per line, with line and token number)
 # OUTPUT OPTIONAL error.txt
 # PARAMETER encoding TYPE [utf8: "UTF-8", latin1: "ISO-8859-1"] DEFAULT utf8
+
+# Kielipankki Chipster offered to show a tokens.tsv result as
+# spreadsheet but got an exception instead. Better to name the result
+# tokens.txt until this works - yes, reported it. (Or produce both
+# results!  Do that!)
 
 # Simple tokenizer to demonstrate the possibilities.
 # Extracts maximal strings of "word characters" as tokens.
@@ -20,11 +26,14 @@ try:
     tokenpattern = re.compile(r'\w+')
 
     with open('text.txt', mode = 'rt', encoding = encoding) as lines, \
-         open('tokens.tsv', mode = 'wt', encoding = encoding) as tokens:
+         open('tokens.txt', mode = 'wt', encoding = encoding) as tokens, \
+         open('tokens.tsv', mode = 'wt', encoding = encoding) as tokenz:
         number = count(start = 1)
         for k, line in enumerate(lines, start = 1):
             for token in tokenpattern.findall(line):
-                print(token, k, next(number), sep = '\t', file = tokens)
+                n = next(number)
+                print(token, k, n, sep = '\t', file = tokens)
+                print(token, k, n, sep = '\t', file = tokenz)
 
 except Exception:
     # Log current exception in error.txt and re-raise
