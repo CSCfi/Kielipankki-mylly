1268d6cdfb89d6b1ca9aed04a00876864dd395d4
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2016-03-08 16:09:39 +0200

    Fixed the right error, unfixed the wrong error
    It was TYPE in OUTPUT, not {...} in OUTPUT name. Come to think
    of it, that's what it said in the logs :/

--- modules/kielipankki/python3/test-multifile.py
+++ modules/kielipankki/python3/test-multifile.py
@@ -1,7 +1,7 @@
 # TOOL test-multifile.py: "Multifile Test" (Receives a pattern of files)
 # INPUT input{...}.txt TYPE GENERIC (Input files, presumably)
 # OUTPUT info.txt  (File attempts to reveal parameters and input files)
-# OUTPUT OPTIONAL output.txt TYPE GENERIC (Guess cannot produce output in pattern)
+# OUTPUT OPTIONAL output{...}.txt (Can haz output in pattern?)
 # OUTPUT OPTIONAL error.txt  (Diagnostics if any)
 # PARAMETER code TYPE [utf8: "UTF-8", latin1: "ISO-8859-1"] DEFAULT utf8
 
@@ -9,9 +9,9 @@
 # - how one gets to set multiple input files in the user interface
 # - how the script gets too see them
 # - how output files appear back in user interface (maybe they do)
-# An output-name pattern doesn't seem to work - at least got SADL error
-# in chipster.log for output{...}.txt (unless it was alredy for the
-# input{...}.txt, that is).
+
+# Output had TYPE - that was the error! Editor accepts patter name,
+# with or without OPTIONAL. http://chipster.csc.fi/tool-editor/
 
 import os, sys
 
