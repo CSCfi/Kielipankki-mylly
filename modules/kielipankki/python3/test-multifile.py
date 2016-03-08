b2d39b68ed647c092e3e3118253a436ad4d6757d
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2016-03-08 14:39:09 +0200

    Attempted SADL fix (no output name {...} pattern allowed?)

--- modules/kielipankki/python3/test-multifile.py
+++ modules/kielipankki/python3/test-multifile.py
@@ -1,7 +1,7 @@
-# TOOL test-multifile.py: "Multifile Test" (Receives and produces a pattern of files)
+# TOOL test-multifile.py: "Multifile Test" (Receives a pattern of files)
 # INPUT input{...}.txt TYPE GENERIC (Input files, presumably)
 # OUTPUT info.txt  (File attempts to reveal parameters and input files)
-# OUTPUT output{...}.txt TYPE GENERIC (Odd-numbered output files)
+# OUTPUT OPTIONAL output.txt TYPE GENERIC (Guess cannot produce output in pattern)
 # OUTPUT OPTIONAL error.txt  (Diagnostics if any)
 # PARAMETER code TYPE [utf8: "UTF-8", latin1: "ISO-8859-1"] DEFAULT utf8
 
@@ -9,6 +9,9 @@
 # - how one gets to set multiple input files in the user interface
 # - how the script gets too see them
 # - how output files appear back in user interface (maybe they do)
+# An output-name pattern doesn't seem to work - at least got SADL error
+# in chipster.log for output{...}.txt (unless it was alredy for the
+# input{...}.txt, that is).
 
 import os, sys
 
