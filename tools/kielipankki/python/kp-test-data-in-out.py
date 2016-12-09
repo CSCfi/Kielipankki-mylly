7ca405365090bc41e7970331c26f077ad7948a36
klemela <klemela>
2016-12-09 15:29:46 +0200

    Remove debug tools and make tool ids unique

---
+++ tools/kielipankki/python/kp-test-data-in-out.py
@@ -0,0 +1,8 @@
+# TOOL kp-test-data-in-out.py: "Test data input and output in Python" (Data input output test.) 
+# INPUT input TYPE GENERIC
+# OUTPUT output
+
+import shutil
+
+shutil.copyfile('input', 'output')
+
