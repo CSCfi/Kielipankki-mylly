67f44d7a09b45b7e0044113d31c28791c4e93457
Martin Matthiesen <martin.matthiesen@csc.fi>
2016-12-19 18:11:07 +0200

    added shell script to be run on test server

---
+++ update_tools.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Update self and restart chipster toolbox (if present)
+
+# cd to dir where script is
+cd `basename $0`
+
+# sync remote changes
+git fetch
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+CHIPSTER_TOOL_RELOAD=/opt/chipster/toolbox/reload-tools.sh
+
+if [ $LOCAL = $REMOTE ]; then
+    : #echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    git pull
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push."
+    echo "This should not happen."
+else
+    echo "Diverged"
+    echo "This should not happen."
+fi
+
+if [ -x $CHIPSTER_TOOL_RELOAD ]; then
+  $CHIPSTER_TOOL_RELOAD
+fi;
