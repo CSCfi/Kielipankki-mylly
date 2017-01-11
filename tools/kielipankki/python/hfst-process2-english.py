aca08be856de91ad39dbf360cc8cd92de65940a3
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-01-11 15:46:46 +0200

    Added new HFST version (3.11.0) to hfst-process*

--- tools/kielipankki/python/hfst-process2-english.py
+++ tools/kielipankki/python/hfst-process2-english.py
@@ -3,7 +3,7 @@
 # OUTPUT segments.txt
 # OUTPUT OPTIONAL error.log
 # PARAMETER Encoding TYPE [utf8: "UTF-8"] DEFAULT utf8 (Character encoding, UTF-8)
-# PARAMETER Version TYPE [v383: "3.8.3", v390: "3.9.0"] DEFAULT v383 (HFST Version)
+# PARAMETER Version TYPE [v383: "3.8.3", v390: "3.9.0", v3110: "3.11.0"] DEFAULT v383 (HFST Version)
 # PARAMETER LineInput TYPE [yes: "yes", no: "no"] DEFAULT no (Whether each line is an input unit. Default separator is an empty line.)
 # PARAMETER PrintAll TYPE [yes: "yes", no: "no"] DEFAULT no (Whether to print nonmatching text, whatever that is. Default not.)
 # PARAMETER PrintWeight TYPE [yes: "yes", no: "no"] DEFAULT no (Whether to print weights. Default not.)
@@ -53,6 +53,26 @@ def process_3_9_0(of):
 
     hfst_process2(*command)
 
-dict(v383 = process_3_8_3, v390 = process_3_9_0)[Version]()
+def process_3_11_0(of):
+    home = "/homeappl/appl_taito/ling/hfst/3.11.0"
+    processor  = os.path.join(home, "bin", "hfst-proc2")
+    transducer = os.path.join(home, "share/hfst/en", "en-analysis.hfst.ol")
+
+    of = dict(xerox = '--xerox',
+              cg = '--cg',
+              segment = '--segment',
+              finnpos = '--finnpos')[OutputFormat]
+
+    command = [processor, of]
+    if LineInput == "yes": command.append('--newline')
+    if PrintAll == "yes": command.append('--print-all')
+    if PrintWeight == "yes": command.append('--print-weight')
+    command.append(transducer)
+
+    hfst_process2(*command)
+
+dict(v383 = process_3_8_3,
+     v390 = process_3_9_0,
+     v3110 = process_3_11_0)[Version]()
 
 consolidate()
