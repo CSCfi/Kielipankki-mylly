d9d831d6ccd25a4796ae8d65fcfd55e446c91162
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-01-11 15:28:53 +0200

    Fix hfst-process2-english

--- tools/kielipankki/python/hfst-process2-english.py
+++ tools/kielipankki/python/hfst-process2-english.py
@@ -14,7 +14,7 @@ import os
 import sys
 
 sys.path.append(os.path.join(chipster_module_path, "python"))
-from lib_pipeline import hfst_process
+from lib_pipeline import hfst_process2
 from lib_errorlog import consolidate
 
 def process_3_8_3():
