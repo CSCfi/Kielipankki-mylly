70bead2115f1262b6e97967ddd86541c0542b3ac
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-01-30 14:48:17 +0200

    Fix mis-match of name in script header with real name of script

--- tools/kielipankki/python/aasr-recognize-wrap.py
+++ tools/kielipankki/python/aasr-recognize-wrap.py
@@ -1,4 +1,4 @@
-# TOOL aasr-wrap-recognize.py: "Aalto ASR Recognize Wrap" (Wraps an audio file for speech recognition in the batch system. Use Aalto ASR Recognize Job on the resulting wrap.)
+# TOOL aasr-recognize-wrap.py: "Aalto ASR Recognize Wrap" (Wraps an audio file for speech recognition in the batch system. Use Aalto ASR Recognize Job on the resulting wrap.)
 # INPUT audio.data TYPE GENERIC
 # OUTPUT data.wrap
 # OUTPUT OPTIONAL error.log
