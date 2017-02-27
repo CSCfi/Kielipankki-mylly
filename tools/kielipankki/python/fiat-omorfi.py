b51301447a78563432b99daf68c1e23d6c6c71ff
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-02-27 11:53:17 +0200

    Testing shows omorfi transducer can be a symlink, so let it be a symlink
    Because why copy then

--- tools/kielipankki/python/fiat-omorfi.py
+++ tools/kielipankki/python/fiat-omorfi.py
@@ -1,6 +1,5 @@
 # TOOL fiat-omorfi.py: "Let there be Omorfi" (Makes the Omorfi transducer appear, suitable for analysis of Finnish word forms)
 # OUTPUT fi-analysis.hfst
-# OUTPUT OPTIONAL fi-analysis.link
 # PARAMETER Version TYPE [v_3_12_1: "3.12.1", v_3_11_0: "3.11.0", v_3_9_0: "3.9.0", v_3_8_3: "3.8.3"] DEFAULT v_3_12_1 (HFST Version)
 # RUNTIME python3
 
@@ -10,5 +9,6 @@ them = dict(v_3_8_3 = '/homeappl/appl_taito/ling/hfst/3.8.3/share/hfst/fi/fi-ana
             v_3_12_1 = '/homeappl/appl_taito/ling/hfst/3.12.1/share/hfst/fi/fi-analysis.hfst.ol')
 
 import os, shutil
-shutil.copy(them[Version], 'fi-analysis.hfst')
-os.symlink(them[Version], 'fi-analysis.link') # see if chipster likes it
+
+# yes, symlink seems fine
+os.symlink(them[Version], 'fi-analysis.hfst')
