3f70ad8e4c55e24491f891945be51a41dbf81aff
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-03-03 09:51:02 +0200

    Added two-level compiler and all manner of transducers (hfst-twolc)
    TWOLC and the transducers should be separate commits but one is too
    lazy to do it now. Too late.
    
    The "language" transducer tools are generated from the omorfi tools by
    a shell script (fiat-more.sh).
    
    Transducers are now a separate section in Mylly.

---
+++ tools/kielipankki/python/fiat-english-g.py
@@ -0,0 +1,14 @@
+# TOOL fiat-english-g.py: "English generation" (Makes an English generation transducer appear)
+# OUTPUT en-generation.hfst
+# PARAMETER Version TYPE [v_3_12_1: "3.12.1", v_3_11_0: "3.11.0", v_3_9_0: "3.9.0", v_3_8_3: "3.8.3"] DEFAULT v_3_12_1 (HFST Version)
+# RUNTIME python3
+
+them = dict(v_3_8_3 = '/homeappl/appl_taito/ling/hfst/3.8.3/share/hfst/en/en-generation.hfst.ol',
+            v_3_9_0 = '/homeappl/appl_taito/ling/hfst/3.9.0/share/hfst/en/en-generation.hfst.ol',
+            v_3_11_0 = '/homeappl/appl_taito/ling/hfst/3.11.0/share/hfst/en/en-generation.hfst.ol',
+            v_3_12_1 = '/homeappl/appl_taito/ling/hfst/3.12.1/share/hfst/en/en-generation.hfst.ol')
+
+import os, shutil
+
+# yes, symlink seems fine
+os.symlink(them[Version], 'en-generation.hfst')
--- tools/kielipankki/python/fiat-finnish-a.py
+++ tools/kielipankki/python/fiat-finnish-a.py
@@ -1,4 +1,4 @@
-# TOOL fiat-omorfi.py: "Let there be Omorfi" (Makes the Omorfi transducer appear, suitable for analysis of Finnish word forms)
+# TOOL fiat-finnish-a.py: "Finnish analysis" (Makes a Finnish analysis transducer appear)
 # OUTPUT fi-analysis.hfst
 # PARAMETER Version TYPE [v_3_12_1: "3.12.1", v_3_11_0: "3.11.0", v_3_9_0: "3.9.0", v_3_8_3: "3.8.3"] DEFAULT v_3_12_1 (HFST Version)
 # RUNTIME python3
