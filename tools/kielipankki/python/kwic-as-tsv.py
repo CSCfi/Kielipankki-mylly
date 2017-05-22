f81480ed3f29c39587f76a92dd8585791ed77ea7
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-05-22 15:10:17 +0300

    Renamed some attributes that we add to each token
    Using kM prefix to indicate numeric (integer) type,
    and replacing other undercores with capitalization
    because R does not play nice with the underscore
    (it kept inserting a stray X before the initial
    underscore. Why is that?)

--- tools/kielipankki/python/kwic-as-tsv.py
+++ tools/kielipankki/python/kwic-as-tsv.py
@@ -22,10 +22,12 @@ kwic = data['kwic']
 # lead sentence/token determines which attributes,
 
 head = list(kwic[0]['tokens'][0]) # lead token
-# also: _match (0/1), _sen (counter, also in meta), _tok (counter within _sen)
+# also: kMmatch (0/1), kMsen (counter, also in meta), kMtok (counter within kMsen)
+# though kMmatch may want to be bMmatch or some such some day (Boolean?)
+# the new naming scheme with these prefixen indicates types to ODS-maker and such
 
 with open('tokens.tmp', mode = 'w', encoding = 'utf-8') as out:
-    print('_match', '_sen', '_tok', *head, sep = '\t', file = out)
+    print('kMmatch', 'kMsen', 'kMtok', *head, sep = '\t', file = out)
     for j, hit in enumerate(kwic):
         for k, token in enumerate(hit['tokens']):
             m = hit['match']
@@ -37,10 +39,12 @@ with open('tokens.tmp', mode = 'w', encoding = 'utf-8') as out:
 os.rename('tokens.tmp', 'tokens.tsv')
 
 meta = list(kwic[0]['structs'])
-# also: _sen (counter, also in head), _start, _end, _corpus
+# also: kMsen (counter, also in tokens), Start, End, Corpus
+# though not sure whether Start, End, Corpus are safe or might also occur
+# as positional in some corpus or other - are they safe? with the Cap?
 
 with open('meta.tmp', mode = 'w', encoding = 'utf-8') as out:
-    print('_sen', '_start', '_end', '_corpus', *meta, sep = '\t', file = out)
+    print('kMsen', 'Start', 'End', 'Corpus', *meta, sep = '\t', file = out)
     for j, hit in enumerate(kwic):
         m, c, data = hit['match'], hit['corpus'], hit['structs']
         print(j, m['start'], m['end'], c, *(data[key] for key in meta),
