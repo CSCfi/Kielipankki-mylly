5f672766e5d8ccaba2ae50ed48d054f222dcb09b
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-05-22 13:36:24 +0300

    A fuller version of TSV as ODS
    Base column types on column name *prefix* - we attempt to do this
    systematically from now on, rely on the user to rename attributes to
    get the benefit of this, and have our own tools produce such names.
    
    cM, kM indicate integer type (count and otherwise)
    wM, vM indicate floating type (weight or otherwise)
    
    That's M for Mylly, there might be further prefixes, and the slight
    sense of weird is an attempt to avoid accidental conflicts. And we
    want it to be short, right?

--- tools/kielipankki/python/tsv-as-ods.py
+++ tools/kielipankki/python/tsv-as-ods.py
@@ -1,64 +1,73 @@
 # TOOL tsv-as-ods.py: "TSV as ODS"
-# (TSV as Open Document Spreadsheet)
+# (TSV as Open Document Spreadsheet. Column name prefix cM or kM indicates that cells contain whole numbers, like counts, and wM or vM indicates a numeric weight.)
 # INPUT table.tsv TYPE GENERIC
 # OUTPUT table.ods
 # RUNTIME python3
 
-# depends on odf (from odfpy)
-
-import odf, os, sys
-from itertools import chain
+import odf, os, sys # depends on odf (package odfpy from PyPI)
 
 sys.path.append(os.path.join(chipster_module_path, "python"))
 import lib_names as names
 
 names.output('table.ods', names.replace('table.tsv', '.ods'))
 
-# extract the writing from
-# https://github.com/eea/odfpy/blob/master/csv2ods/csv2ods which
-# mainly deals with Python2 compatibility, option parsing, reading
-# CSV, and guessing character encodings and cell styles :/ - now
-# adding header row on one's own
 from odf.opendocument import OpenDocumentSpreadsheet
-from odf.style import Style, TextProperties, ParagraphProperties, TableColumnProperties
-from odf.text import P
-from odf.table import Table, TableColumn, TableHeaderRows, TableRow, TableCell
+from odf.table import Table, TableHeaderRows, TableRow, TableCell
 
-textdoc = OpenDocumentSpreadsheet()
+sheet = OpenDocumentSpreadsheet()
 
-# Create a style for the table content. One we can modify later in the
-# word processor. [Copied this from csv2ods - is this even useful?
-# does there need to be a style? find out!]
-tablecontents = Style(name="Table Contents", family="paragraph")
-tablecontents.addElement(ParagraphProperties(numberlines="false", linenumber="0"))
-tablecontents.addElement(TextProperties(fontweight="bold"))
-textdoc.styles.addElement(tablecontents)
+# We would like to attach "common" styles for different kinds of data
+# columns in the table (and for head cells, why not) for the user to
+# modify in their ODS "consumer", but the principal consumer does not
+# seem to support common styles properly. (Maybe the latest version,
+# though? Or a future version? Or some other consumer altogether?)
+
+# See https://github.com/eea/odfpy/blob/master/csv2ods/csv2ods for how
+# one attaches common styles to a spreadsheet document in odfpy. (It
+# deals mainly with Python2 compatibility, option parsing, reading
+# CSV, and guessing character encodings and cell styles :/ Oh well.)
 
 table = Table(name = 'table')
 
+def floatcell(value):
+    # should have a number style in table:column to make it show as a
+    # "number" or a "decimal number" or a "scientific number" but for
+    # now, the principal consumer may not use it right; on the other
+    # hand, it seems to guess nicely, hopefully, maybe
+    return TableCell(valuetype = "float", value = value)
+
+def stringcell(value):
+    # header table:table-row should also have a suitable default style
+    return TableCell(valuetype = "string", stringvalue = value)
+
+def row(headmap, record):
+    result = TableRow()
+    for make, value in zip(headmap, record):
+        result.addElement(make(value))
+    return result
+
 with open('table.tsv', encoding = 'utf-8') as tsv:
-    # header
+    # header from first line
     head = next(tsv).rstrip('\n').split('\t')
-    hrs = TableHeaderRows()
-    table.addElement(hrs) # do these _need_ to be added before they are done?
-    hr = TableRow()
-    hrs.addElement(hr)
+    # (this is where we would insert default cell styles for the
+    # columns)
+    headrows = TableHeaderRows()
+    headrow = TableRow()
     for name in head:
-        hc = TableCell(valuetype = "string")
-        hr.addElement(hc)
-        p = P(stylename = tablecontents, text = name) # is this right?
-        hc.addElement(p)
+        headrow.addElement(stringcell(name))
+    headrows.addElement(headrow)
+    table.addElement(headrows)
+    # empty row before data rows helps select data rows at least for
+    # sorting at least in an oldish LibreOffice (one can hide it)
+    table.addElement(TableRow())
     # records
+    headmap = [ (floatcell if name.startswith(('cM', 'kM', 'wM', 'vM'))
+                 else stringcell)
+                for name in head ]
     for line in tsv:
         record = line.rstrip('\n').split('\t')
-        tr = TableRow()
-        table.addElement(tr)
-        for value in record:
-            tc = TableCell(valuetype = "string")
-            tr.addElement(tc)
-            p = P(stylename = tablecontents, text = value) # right?
-            tc.addElement(p)
+        table.addElement(row(headmap, record))
 
-textdoc.spreadsheet.addElement(table)
-textdoc.save('table.tmp')
+sheet.spreadsheet.addElement(table)
+sheet.save('table.tmp')
 os.rename('table.tmp', 'table.ods')
