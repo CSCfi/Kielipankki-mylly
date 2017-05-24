0509d26b02b21adbb2ee3546cf7b937a34158638
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-05-24 10:45:15 +0300

    Make Korp query have random seed (default random) and page
    User needs to repeat the query specifically to get the next page, to
    not hammer on Korp unnecessarily, but when they want that, they can
    get that, within limits. Korp apparently does not cache the search
    results for mere fetching of further results, so it will repeat the
    whole CQP search.

--- tools/kielipankki/python/kwic-from-suomi24.py
+++ tools/kielipankki/python/kwic-from-suomi24.py
@@ -1,17 +1,20 @@
 # TOOL kwic-from-suomi24.py: "Concordance from Suomi24 corpus in Korp"
-# (Queries Korp for a concordance from Suomi24 corpus. Input file contains CQP expressions, separated by empty lines, that must all match, and the last expression defines the final match region. Output file is the concordance in a JSON form.)
-# INPUT cqps.txt TYPE GENERIC
-# OUTPUT conc.json
+# (Queries Korp for a KWIC concordance from Suomi24 corpus. Input file contains CQP expressions, separated by empty lines, that must all match, and the last expression defines the final match region. Output file is the concordance in a JSON form.)
+# INPUT query.txt TYPE GENERIC
+# OUTPUT result.json
+# PARAMETER OPTIONAL seed: "Random seed" TYPE INTEGER FROM 1000 TO 9999 (Use the same seed to repeat the same ordering of the results.)
+# PARAMETER page: "Concordance page" TYPE INTEGER FROM 0 TO 9 DEFAULT 0 (Extract the specified page of 1000 results from the concordance.)
 # RUNTIME python3
 
 from itertools import count, groupby
-import json
+import json, random
 import requests
 
 sys.path.append(os.path.join(chipster_module_path, "python"))
 import lib_names as names
 
-names.output('conc.json', names.replace('cqps.txt', '.json'))
+seed = seed or random.randrange(10 ** 4, 10 ** 5)
+names.output('result.json', names.replace('query.txt', '-s{}-p{}.json'.format(seed, page)))
 
 KORP = 'https://korp.csc.fi/cgi-bin/korp.cgi'
 
@@ -20,10 +23,10 @@ KORP = 'https://korp.csc.fi/cgi-bin/korp.cgi'
 # in an input file. The tool can know what attributes are appropriate
 # to its corpus.
 
-with open('cqps.txt') as cqps:
+with open('query.txt') as query:
     ooKEY = ( ('cqp{}'.format(k) if k else 'cqp') for k in count() )
     ooCQP = ( ' '.join(lines).strip()
-              for between, lines in groupby(cqps, str.isspace)
+              for between, lines in groupby(query, str.isspace)
               if not between )
     QUERYZ = dict(zip(ooKEY, ooCQP))
 
@@ -33,14 +36,14 @@ META = 'sentence_id,text_title,text_date,text_time,text_sect,text_sub,text_user,
 
 # https://www.kielipankki.fi/support/korpapi/
 
-it = dict(command='query',
+it = dict(command = 'query',
           corpus = CORPUS,
           sort = 'random',
-          # random_seed = '1278',
-          defaultcontext='1 sentence',
-          defaultwithin='sentence',
-          start='0',
-          end='999',
+          random_seed = seed,
+          defaultcontext = '1 sentence',
+          defaultwithin = 'sentence',
+          start = page * 1000,
+          end = (page + 1) * 1000 - 1,
           show = ANNO,
           show_struct = META)
 
@@ -54,13 +57,13 @@ it.update(QUERYZ)
 # make it optional (with or without a default), or at least
 # provide an informative error message.
 
-r = requests.get(KORP, params = it, timeout = 10.0)
+r = requests.get(KORP, params = it, timeout = 30.0)
 r.raise_for_status()
 
-# response can be an error message in JSON, or a concordance;
-# to detect error message, so only a concordance goes to file
+# response can be an error message in JSON, or a concordance; shoul
+# detect error message here, so only a concordance goes to file
 
-with open('conc.json', mode = 'w', encoding = 'utf-8') as conc:
-    json.dump(r.json(), conc,
+with open('result.json', mode = 'w', encoding = 'utf-8') as result:
+    json.dump(r.json(), result,
               ensure_ascii = False,
               check_circular = False)
