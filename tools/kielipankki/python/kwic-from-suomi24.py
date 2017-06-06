beb4c74a6597771cb3b5df8f361c91e8792d43c0
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-06-06 15:20:35 +0300

    Turned Korp API call into a library
    In anticipation of many KWIC tools that only specify the
    corpus particulars before calling the generic library
    
    Wonder what slip of fingers is revealed on first test now :/

--- tools/kielipankki/python/kwic-from-suomi24.py
+++ tools/kielipankki/python/kwic-from-suomi24.py
@@ -3,75 +3,46 @@
 # INPUT query.txt TYPE GENERIC
 # OUTPUT result.json
 # PARAMETER OPTIONAL seed: "Random seed" TYPE INTEGER FROM 1000 TO 9999 (Use the same seed to repeat the same ordering of the results.)
-# PARAMETER page: "Concordance page" TYPE INTEGER FROM 0 TO 9 DEFAULT 0 (Extract the specified page 0-9, of 1000 results each, from the concordance.)
+# PARAMETER page: "Concordance page" TYPE INTEGER FROM 0 TO 9 DEFAULT 0 (Extract the specified page, 0-9, of up to 1000 results each, from the concordance.)
 # RUNTIME python3
 
-from itertools import count, groupby
-from math import isnan
-import json, random
-import requests
+# This tool specifies attributes for a particular corpus.
+
+import json, math, random
 
 sys.path.append(os.path.join(chipster_module_path, "python"))
+from lib_korp import parse_query, request_kwic
 import lib_names as names
 
-seed = random.randrange(1000, 10000) if isnan(seed) else seed
-names.output('result.json', names.replace('query.txt', '-s{}-p{}.json'.format(seed, page)))
+seed = random.randrange(1000, 10000) if math.isnan(seed) else seed
+names.output('result.json', names.replace('query.txt', '-s{}p{}.json'.format(seed, page)))
 
-KORP = 'https://korp.csc.fi/cgi-bin/korp.cgi'
-
-# Make this be for Suomi24, and another tool for another set of
-# corpora. Corpus selection is not a parameter at all. The queries are
-# in an input file. The tool can know what attributes are appropriate
-# to its corpus.
-
-with open('query.txt') as query:
-    ooKEY = ( ('cqp{}'.format(k) if k else 'cqp') for k in count() )
-    ooCQP = ( ' '.join(lines).strip()
-              for between, lines in groupby(query, str.isspace)
-              if not between )
-    QUERYZ = dict(zip(ooKEY, ooCQP))
+comma = ','
 
 CORPUS = 'S24'
-ANNO = 'lemma,pos,msd,dephead,deprel,ref,lex,nertag'
-META = 'sentence_id,text_title,text_date,text_time,text_sect,text_sub,text_user,sentence_id,text_urlmsg,text_urlboard'
 
-# https://www.kielipankki.fi/support/korpapi/
+ANNO = comma.join('lemma pos msd dephead deprel ref lex nertag'.split())
 
-it = dict(command = 'query',
-          corpus = CORPUS,
-          sort = 'random',
-          random_seed = seed,
-          defaultcontext = '1 sentence',
-          defaultwithin = 'sentence',
-          start = page * 1000,
-          end = (page + 1) * 1000 - 1,
-          show = ANNO,
-          show_struct = META)
+META = comma.join('''
 
-it.update(QUERYZ)
+    sentence_id
+    text_title  text_date  text_time
+    text_sect  text_sub  text_user
+    text_urlmsg  text_urlboard
 
-# Jira this!  looks like (1) show_struct is not optional and (2)
-# cannot be empty and (3) the error message for it is "string
-# index out of bounds" which is not good -- minimally fix doc [?+]
-# --> [+], but it's a bit funny to require that user require
-# structural attributes in the result, so a better fix would be to
-# make it optional (with or without a default), or at least
-# provide an informative error message.
+    '''.split())
 
-r = requests.get(KORP, params = it, timeout = 30.0)
-r.raise_for_status()
+QUERIES = parse_queries('query.txt')
 
-it = r.json()
-
-if 'ERROR' in it:
-    print(it['ERROR']['type'], it['ERROR']['message'], file = sys.stderr)
-    exit(1)
-
-if it['hits'] == 0:
-    print('empty concordance (no positional names)', file = sys.stderr)
-    exit(1)
+kwic = request_kwic(corpus = CORPUS,
+                    seed = seed,
+                    size = 1000,
+                    page = page,
+                    anno = ANNO,
+                    meta = META,
+                    queries = QUERIES)
 
 with open('result.json', mode = 'w', encoding = 'utf-8') as result:
-    json.dump(it, result,
+    json.dump(kwic, result,
               ensure_ascii = False,
               check_circular = False)
---
+++ tools/kielipankki/python/lib_korp.py
@@ -0,0 +1,68 @@
+from itertools import count, groupby
+import json
+import requests
+
+KORP = 'https://korp.csc.fi/cgi-bin/korp.cgi'
+
+def parse_queries(filename):
+    '''Dict of line-separated queries from the file, keyed with cqp, cqp1, ...'''
+    with open(filename) as query:
+        ooKEY = ( ('cqp{}'.format(k) if k else 'cqp') for k in count() )
+        ooCQP = ( ' '.join(lines).strip()
+                  for between, lines in groupby(query, str.isspace)
+                  if not between )
+        return dict(zip(ooKEY, ooCQP))
+
+
+def request_kwic(*,
+                 corpus,
+                 seed,
+                 size = 1000,
+                 page,
+                 anno,
+                 meta,
+                 queries):
+
+    '''Query KORP for a KWIC, see
+    https://www.kielipankki.fi/support/korpapi/.
+
+    Return the KWIC as a dict.
+
+    Halt execution on error response or empty concordance. The latter
+    is because in an empty concordance positional attributes are not
+    named, and the result can then not be transformed into a tabular
+    form that is compatible with non-empty tables from the same
+    source.
+
+    '''
+
+    it = dict(command = 'query',
+              corpus = corpus,
+              sort = 'random',
+              random_seed = seed,
+              defaultcontext = '1 sentence',
+              defaultwithin = 'sentence',
+              start = page * size,
+              end = (page + 1) * size - 1,
+              show = anno,
+              show_struct = meta)
+    
+    it.update(queries)
+
+    response = requests.get(KORP, params = it, timeout = 30.0)
+    response.raise_for_status()
+
+    result = response.json()
+
+    if 'ERROR' in result:
+        print(result['ERROR']['type'],
+              result['ERROR']['message'],
+              file = sys.stderr)
+        exit(1)
+
+    if result['hits'] == 0:
+        print('empty concordance (no positional names)',
+              file = sys.stderr)
+        exit(1)
+
+    return result
