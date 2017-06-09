927504e1d5b3234ac81d1fecad1028b65de53b3c
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-06-09 08:46:12 +0300

    Linear Zipf smoother, for a straighter plot line
    From Sampson's article,	citation needed; tested	off-chipster

---
+++ tools/kielipankki/python/tsv-smooth-zipf.py
@@ -0,0 +1,48 @@
+# TOOL tsv-smooth-zipf.py: "Extend Zipfian TSV with smoothed species count"
+# (Extend a map of observation count to the species count with a linearly smoothed species count, from Sampson)
+# INPUT counts.tsv TYPE GENERIC
+# OUTPUT smooth.tsv
+# PARAMETER observations TYPE STRING DEFAULT "observations"
+# PARAMETER species TYPE STRING DEFAULT "species"
+# PARAMETER smooth TYPE STRING DEFAULT "species1"
+# RUNTIME python3
+
+import os, sys
+from itertools import chain
+
+sys.path.append(os.path.join(chipster_module_path, "python"))
+import lib_names as names
+
+names.enforce('counts.tsv', '.tsv')
+names.output('smooth.tsv', names.replace('counts.tsv', '-smooth.tsv'))
+
+with open('counts.tsv') as counts:
+    head = next(counts).rstrip('\n').split('\t')
+    OBS = head.index(observations)
+    SPE = head.index(species)
+    data = sorted((int(record[OBS]),
+                   int(record[SPE]),
+                   record)
+                  for record in (line.rstrip('\n').split('\t')
+                                 for line in counts))
+
+ps = (obs for obs, spe, rec in data)
+ns = (spe for obs, spe, rec in data)
+qs = (obs for obs, spe, rec in data)
+
+ps = chain([0], ps)
+
+pen, _, _ = data[-2]
+ult, _, _ = data[-1]
+qs = chain(qs, [2*ult - pen])
+next(qs)
+
+zs = (2*n/(q - p) for p, n, q in zip(ps, ns, qs))
+rs = (rec for obs, spe, rec in data)
+
+with open('smooth.tmp', mode = 'w') as out:
+    print(smooth, *head, sep = '\t', file = out)
+    for z, r in zip(zs, rs):
+        print(z, *r, sep = '\t', file = out)
+
+os.rename('smooth.tmp', 'smooth.tsv')
