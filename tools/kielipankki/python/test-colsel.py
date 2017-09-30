05181ccf8427ab21dafd531bde39006cc04121e9
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-09-30 16:23:24 +0300

    Comment on test result in test tool

--- tools/kielipankki/python/test-colsel.py
+++ tools/kielipankki/python/test-colsel.py
@@ -1,7 +1,8 @@
 # TOOL test-colsel.py: "Test COLUMN_SEL parameter"
 #     (Test Chipster tool interface parameter type COLUMN_SEL,
 #      now with two required files to see if union or intersection
-#      of names becomes available, or what.)
+#      of names becomes available, or what. Ok it only show the
+#      first set. Good enough?)
 # INPUT some.tsv TYPE GENERIC (Whatever TSV file. TSV file has column names.)
 # INPUT more.tsv TYPE GENERIC (Whatever another TSV file.)
 # OUTPUT info.txt  (File attempts to reveal parameters)
