50282bd2786704ff39a1106b60d265953bfcf29f
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-09-30 15:52:46 +0300

    Tool to test COLUMN_SEL parameter type
    Accidentally testing on master but never mind

---
+++ tools/kielipankki/python/test-colsel.py
@@ -0,0 +1,18 @@
+# TOOL test-colsel.py: "Test COLUMN_SEL parameter"
+#     (Test Chipster tool interface parameter type COLUMN_SEL)
+# INPUT some.tsv TYPE GENERIC (Whatever TSV file. TSV file has column names.)
+# INPUT OPTIONAL more.tsv TYPE GENERIC (Whatever another TSV file.)
+# OUTPUT info.txt  (File attempts to reveal parameters)
+# PARAMETER col1: "First column name" (Column in input file)
+# PARAMETER col2: "Nothe column name" (Column in input file)
+# PARAMETER col3: "Anoth column name" (Column in input file)
+# RUNTIME python3
+
+# Want to run this in Chipster and see
+# - how one gets to select column name in the user interface
+
+with open('info.txt', 'wt') as f:
+    print('one column:', repr(col1), file = f)
+    print('two column:', repr(col2), sep = '\t', file = f)
+    print('yet anothe:', repr(col3), sep = '\t', file = f)
+    print(file = f)
