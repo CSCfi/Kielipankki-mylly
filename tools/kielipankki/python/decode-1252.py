343f5c29d49bbfc7f005cf17ef6bf798319b14d5
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2017-11-12 16:06:08 +0200

    Decoder of Windows-1252
    New plan is to have UTF-8 as implicit target, transcoding tools be
    really simple to run with no parameters at all
    
    There appears to be no way to prevent transcoding even such codes as
    are not part of the source code, though? Not sure. On the other hand,
    the real problem is control codes that are in some sense valid input.

---
+++ tools/kielipankki/python/decode-1252.py
@@ -0,0 +1,32 @@
+# TOOL decode-1252.py: "Decode Windows-1252" (Convert Windows-1252 encoding to UTF-8)
+# INPUT data.txt TYPE GENERIC
+# OUTPUT text.txt
+# RUNTIME python3
+
+import os, sys
+from subprocess import Popen, DEVNULL
+
+sys.path.append(os.path.join(chipster_module_path, "python"))
+from lib_names2 import name, base
+
+name('text.txt', base('data.txt', '*.txt'),
+     ins = 'utf-8',
+     ext = 'txt')
+
+with Popen(['iconv', '-f', 'windows-1252', '-t', 'utf-8'],
+           stdin = open('data.txt', 'rb'),
+           stdout = open('text.tmp', 'wb'),
+           stderr = DEVNULL) as iconv:
+    pass # wait
+
+if iconv.returncode is None:
+    print('iconv did not finish', file = sys.stderr)
+    iconv.kill()
+    exit(1)
+
+if iconv.returncode:
+    print('iconv returned with {}'.format(iconv.returncode),
+          file = sys.stderr)
+    exit(1)
+
+os.rename('text.tmp', 'text.tsv')
