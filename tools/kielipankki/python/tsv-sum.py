7d17c131b767c5617d7d2a8624ed06a0a5dcfc6e
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2018-01-26 21:21:34 +0200

    Made sum use existing tag field if any
    Accidentally developing on master but also tested off-Mylly first

--- tools/kielipankki/python/tsv-sum.py
+++ tools/kielipankki/python/tsv-sum.py
@@ -1,38 +1,40 @@
 # TOOL tsv-sum.py: "Sum of relations"
-# (Makes sum aka disjoint union of relations of same type. Tags each record to separate the sources.)
-# INPUT one.tsv TYPE GENERIC
-# INPUT OPTIONAL two1.tsv TYPE GENERIC
-# INPUT OPTIONAL two2.tsv TYPE GENERIC
-# INPUT OPTIONAL two3.tsv TYPE GENERIC
-# INPUT OPTIONAL two4.tsv TYPE GENERIC
+# (Makes sum aka disjoint union of relations that have the same type modulo optional tag field. Tags or re-tags each record to separate the sources.)
+# INPUT input0.tsv TYPE GENERIC
+# INPUT OPTIONAL input1.tsv TYPE GENERIC
+# INPUT OPTIONAL input2.tsv TYPE GENERIC
+# INPUT OPTIONAL input3.tsv TYPE GENERIC
+# INPUT OPTIONAL input4.tsv TYPE GENERIC
+# INPUT OPTIONAL input5.tsv TYPE GENERIC
+# INPUT OPTIONAL input6.tsv TYPE GENERIC
+# INPUT OPTIONAL input7.tsv TYPE GENERIC
+# INPUT OPTIONAL input8.tsv TYPE GENERIC
+# INPUT OPTIONAL input9.tsv TYPE GENERIC
+# INPUT OPTIONAL inputA.tsv TYPE GENERIC
+# INPUT OPTIONAL inputB.tsv TYPE GENERIC
+# INPUT OPTIONAL inputC.tsv TYPE GENERIC
+# INPUT OPTIONAL inputD.tsv TYPE GENERIC
+# INPUT OPTIONAL inputE.tsv TYPE GENERIC
+# INPUT OPTIONAL inputF.tsv TYPE GENERIC
 # OUTPUT sum.tsv
 # PARAMETER tag: "tag field" TYPE STRING DEFAULT "kMtag"
 # RUNTIME python3
 
-from itertools import chain
+from collections import defaultdict
+from itertools import count
 from glob import glob
 import os, sys
 
 sys.path.append(os.path.join(chipster_module_path, "python"))
 from lib_names2 import base, name
 
-for rest in glob('two?.tsv'): base(rest, '*.rel.tsv')
-name('sum.tsv', base('one.tsv', '*.rel.tsv'),
+for rest in glob('input?.tsv'): base(rest, '*.rel.tsv')
+name('sum.tsv', base('input0.tsv', '*.rel.tsv'),
      ins = 'sum',
      ext = 'rel.tsv')
 
 def index(head, names): return tuple(map(head.index, names))
-# def share(head, head2): return tuple(set(head) & set(head2))
-# def other(head, names): return tuple(set(head) - set(names))
 def value(record, ks): return tuple(record[k] for k in ks)
-
-def checktag(tag, head):
-    if tag in head:
-        print("tag name already in use:", repr(tag),
-              file = sys.stderr)
-        print("choose any that is not one of these:",
-              *map(repr, head), sep = '\n', file = sys.stderr)
-        exit(1)
 
 def checktype(ones, twos):
     oneset, twoset = set(ones), set(twos)
@@ -43,22 +45,32 @@ def checktype(ones, twos):
     print('in both:', *(oneset & twoset), file = sys.stderr)
     exit(1)
 
+def mod(names):
+    '''modulo tag field'''
+    return [ name for name in names if name != tag ]
+
 with open('sum.tmp', mode = 'w', encoding = 'utf-8') as out:
-    with open('one.tsv', encoding = 'utf-8') as fin1:
-        onehead = next(fin1).rstrip('\n').split('\t')
-        checktag(tag, onehead)
-        print(tag, *onehead, sep = '\t', file = out)
-        for record in (line.rstrip('\n').split('\t') for line in fin1):
-            # overkill
-            print(1, *record, sep = '\t', file = out)
-        for k, two in enumerate(glob('two?.tsv'), start = 2):
-            with open(two, encoding = 'utf-8') as fin2:
-                twohead = next(fin2).rstrip('\n').split('\t')
-                checktype(onehead, twohead)
-                twindex = index(twohead, onehead)
-                for record in (value(line.rstrip('\n').split('\t'), twindex)
-                               for line in fin2):
-                    # not overkill
-                    print(k, *record, sep = '\t', file = out)
+    onehead = next(open('input0.tsv', encoding = 'UTF-8')).rstrip('\n').split('\t')
+    print(tag, *mod(onehead), sep = '\t', file = out)
+
+    tic = count(start = 1)
+    for inf in sorted(glob('input?.tsv')):
+        with open(inf, encoding = 'utf-8') as fin:
+            twohead = next(fin).rstrip('\n').split('\t')
+            checktype(mod(onehead), mod(twohead))
+            twindex = index(twohead, mod(onehead))
+
+            if tag in twohead:
+                # relation already has tags
+                def get(record, *, k = twohead.index(tag)): return record[k]
+            else:
+                # provide constant tag for relation
+                def get(record): return '1'
+            
+            tac = defaultdict(lambda : next(tic))
+            for t, record in ((tac[get(record)], value(record, twindex))
+                              for line in fin
+                              for record in [line.rstrip('\n').split('\t')]):
+                print(t, *record, sep = '\t', file = out)
 
 os.rename('sum.tmp', 'sum.tsv')
