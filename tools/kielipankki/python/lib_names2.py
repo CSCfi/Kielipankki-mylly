bc30d565565642b9dc7937c6d0d2de6808e07c3d
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2018-03-17 17:37:04 +0200

    Allow alternative input file name patterns
    python/lib_names2.py
        before: lib_names2.base(name, pattern)
        after:  lib.names2.base(internal, pattern, ...)

--- tools/kielipankki/python/lib_names2.py
+++ tools/kielipankki/python/lib_names2.py
@@ -3,8 +3,9 @@ from fnmatch import fnmatch
 
 # https://github.com/chipster/chipster/wiki/TechnicalManual#output-file-names
 
-# Usage in a Mylly tool script: enforce that an input display name matches a
-# pattern, and base output display names on their first components:
+# Usage in a Mylly tool script: enforce that an input display name
+# matches a pattern, and base output display name on the first
+# component of input name:
 #
 # base('input.ext', '*.foo.bar')
 #   => 'ensin' if input.ext is like 'ensin.jotain-jotain.foo.bar'
@@ -12,21 +13,26 @@ from fnmatch import fnmatch
 #   => error message otherwise
 #
 # name('output.ext', base('input.ext', '*'), ins = '1', ext = 'svg')
-#   maps output.ext => ensin.1.svg in chipster-outputs.tsv
+#   maps input.ext => ensin in chipster-inputs.tsv
+#   to output.ext => ensin.1.svg in chipster-outputs.tsv
 #   (ins and ext are keyword only, ins is optional)
 #
 # name('output.ext', 'constant', ext = 'svg')
 #   maps output.ext => constant.svg in chipster-outputs.tsv
 
-def base(argname, pattern, *, cash = None):
+cash = None
+
+def base(argname, *patterns):
     '''Enforces that there is a corresponding input
-    name in chipster_inputs.tsv, it matches the
+    name in chipster_inputs.tsv, it matches a
     pattern, and it has a first component.
 
     Returns the first component of the input
     name.
 
     '''
+
+    global cash
     if cash is None:
         try:
             with open('chipster-inputs.tsv', 'r',
@@ -50,11 +56,15 @@ def base(argname, pattern, *, cash = None):
         exit(1)
     
     name = cash[argname]
-    if fnmatch(name, pattern):
-        base = name.split('.', 1)[0]
+    for pattern in patterns:
+        if fnmatch(name, pattern):
+            base = name.split('.', 1)[0]
+            break
+        else:
+            continue
     else:
         print('input file name:', name, file = sys.stderr)
-        print('does not match:', pattern, file = sys.stderr)
+        print('does not match:', ', '.join(patterns), file = sys.stderr)
         exit(1)
 
     if not base:
