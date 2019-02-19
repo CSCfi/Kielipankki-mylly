ece46f5864222326c74ba67e02b4d6108662465c
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-02-19 15:47:04 +0200

    Attempt to see why vrt-finer stalls

--- tools/kielipankki/python/vrt-finer.py
+++ tools/kielipankki/python/vrt-finer.py
@@ -1,11 +1,11 @@
-# TOOL vrt-finer.py: "Nertag Finnish VRT with FiNER" (Recognize and tag names and such in Finnish VRT.)
+# TOOL vrt-finer.py: "Nertag Finnish VRT with FiNER" (Recognize and classify names and such in Finnish VRT.)
 # INPUT input.vrt TYPE GENERIC
 # OUTPUT output.vrt
 # OUTPUT error.log
 # RUNTIME python3
 
 import os, sys
-from subprocess import Popen
+from subprocess import Popen, TimeoutExpired
 
 # VRT tools are not yet installed in a proper place
 # so Mylly has some temporary copies
@@ -19,15 +19,20 @@ name('output.vrt', base('input.vrt', '*.vrt.txt'),
      ext = 'vrt.txt')
 
 try:
-    with Popen(['python3', PROG],
-               stdin = open('input.vrt', mode = 'rb'),
-               stdout = open('output.tmp', mode = 'wb'),
-               stderr = open('error.log', mode = 'wb')) as fine:
-        pass
-    
+    fine = Popen(['python3', PROG],
+                 stdin = open('input.vrt', mode = 'rb'),
+                 stdout = open('output.tmp', mode = 'wb'),
+                 stderr = open('error.log', mode = 'wb'))
+    status = fine.wait(20)
+
+except TimeoutExpired as exn:
+    # just so there is output.vrt so chipster let one see error.log,
+    # hopefully to find out why the job stalls over there
     os.rename('output.tmp', 'output.vrt')
-    
+
 except Exception as exn:
     et, ev, tr = sys.exc_info()
     print(et, ev, tr, sep = '\n', file = sys.stderr)
     exit(2)
+
+os.rename('output.tmp', 'output.vrt')
