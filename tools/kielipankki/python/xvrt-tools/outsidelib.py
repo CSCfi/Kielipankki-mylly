87b4229d3ca127482c3b713b9ae167a7891c4f15
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-02-19 13:29:28 +0200

    Added to temporary vrt-tools
    These are copies from the konversio repo where vrt-tools actually
    live, here for the installation of some udpipe and finnish-tagtools
    tools in Mylly
    
    xvrt-tools/hrt-tokenize-finnish-hfst
    xvrt-tools/vrt-finer
    xvrt-tools/vrt-finpos
    xvrt-tools/vrt-udpipe
    xvrt-tools/outsidelib.py

---
+++ tools/kielipankki/python/xvrt-tools/outsidelib.py
@@ -0,0 +1,23 @@
+MARMOTJAR = '/proj/kieli/varpunen/marmot-2014-10-22.jar'
+
+MARMOTMODEL = '/proj/kieli/varpunen/models/fin_model.marmot'
+
+UDPIPE = '/appl/ling/udpipe/1.2.0/bin/udpipe'
+
+UDPIPEMODEL = (
+    '/appl/ling/udpipe/models/udpipe-ud-2.3-181115/{}-2.3-181115.udpipe'
+)
+
+HFSTTOKENIZE = '/appl/ling/hfst/3.15.0/bin/hfst-tokenize'
+OMORFITOKENIZE = (
+    '/appl/ling/finnish-tagtools/1.3.2/share/finnish-tagtools'
+    '/omorfi_tokenize.pmatch'
+)
+
+FINER = (
+    '/appl/ling/finnish-tagtools/1.3.2/bin/finnish-nertag'
+)
+
+FINPOS =  (
+    '/appl/ling/finnish-tagtools/1.3.2/bin/finnish-postag'
+)
