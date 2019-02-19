4d96c93a97e57662383929299bf8ad820f58c344
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-02-19 16:57:37 +0200

    Clean up draft vrt-finer.py
    See if chipster will simply show stderr upon cancel,
    assuming the tool still hangs.
    
    The problems with finnish-nertag appear to be two:
    - it will not see hfst-lookup over there (to have vrt-finer set PATH)
    - finnpos appears to expect ascii over there and so fails
    
    (Without appropriate PATH finnish-nertag is also not using system
    hfst-tokenize but its own. But then it fails to find hfst-lookup.)

--- tools/kielipankki/python/vrt-finer.py
+++ tools/kielipankki/python/vrt-finer.py
@@ -1,11 +1,10 @@
 # TOOL vrt-finer.py: "Nertag Finnish VRT with FiNER" (Recognize and classify names and such in Finnish VRT.)
 # INPUT input.vrt TYPE GENERIC
 # OUTPUT output.vrt
-# OUTPUT error.log
 # RUNTIME python3
 
 import os, sys
-from subprocess import Popen, TimeoutExpired
+from subprocess import Popen
 
 # VRT tools are not yet installed in a proper place
 # so Mylly has some temporary copies
@@ -18,22 +17,10 @@ from lib_names2 import base, name
 name('output.vrt', base('input.vrt', '*.vrt.txt'),
      ext = 'vrt.txt')
 
-try:
-    fine = Popen(['python3', PROG],
-                 stdin = open('input.vrt', mode = 'rb'),
-                 stdout = open('output.tmp', mode = 'wb')
-                 # stderr = open('error.log', mode = 'wb')
-    )
-    status = fine.wait(20)
+fine = Popen(['python3', PROG],
+             stdin = open('input.vrt', mode = 'rb'),
+             stdout = open('output.tmp', mode = 'wb'))
 
-except TimeoutExpired as exn:
-    # just so there is output.vrt so chipster let one see error.log,
-    # hopefully to find out why the job stalls over there
-    os.rename('output.tmp', 'output.vrt')
-
-except Exception as exn:
-    et, ev, tr = sys.exc_info()
-    print(et, ev, tr, sep = '\n', file = sys.stderr)
-    exit(2)
+status = fine.wait()
 
 os.rename('output.tmp', 'output.vrt')
