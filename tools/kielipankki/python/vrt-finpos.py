226b11f0d929ce46b1abf8be35fd3f9870d708ae
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-02-20 12:37:12 +0200

    Draft FinnPos
    ../kielipankki-module.xml
    vrt-finer.py
    vrt-finpos.py
    xvrt-tools/vrt-finpos

--- tools/kielipankki/python/vrt-finpos.py
+++ tools/kielipankki/python/vrt-finpos.py
@@ -1,4 +1,4 @@
-# TOOL vrt-finer.py: "Classify names in Finnish VRT with FiNER" (Classify names in Finnish VRT.)
+# TOOL vrt-finpos.py: "Postag Finnish VRT with FinnPos" (Classify words in Finnish VRT.)
 # INPUT input.vrt TYPE GENERIC
 # OUTPUT output.vrt
 # RUNTIME python3
@@ -9,7 +9,7 @@ from subprocess import Popen
 # VRT tools are not yet installed in a proper place
 # so Mylly has some temporary copies
 PROG = os.path.join(chipster_module_path, 'python', 'xvrt-tools',
-                    'vrt-finer')
+                    'vrt-finpos')
 
 sys.path.append(os.path.join(chipster_module_path, "python"))
 from lib_names2 import base, name
@@ -17,11 +17,11 @@ from lib_names2 import base, name
 name('output.vrt', base('input.vrt', '*.vrt.txt'),
      ext = 'vrt.txt')
 
-nertag = Popen(['python3', PROG],
+postag = Popen(['python3', PROG],
                stdin = open('input.vrt', mode = 'rb'),
                stdout = open('output.tmp', mode = 'wb'))
 
-status = nertag.wait()
+status = postag.wait()
 status and exit(status)
 
 os.rename('output.tmp', 'output.vrt')
