c10132ee30c4cc7f4f65756aab7cb539db7e7b0a
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-04-14 20:21:53 +0300

    Update temporary xvrt-tools
    xvrt-tools/update.sh
    - new update script
    - expects current konversio repo to be there
    
    hrt-tokenize-finnish-hfst
    hrt-tokenize-udpipe
    outsidelib.py
    vrt-finer
    vrt-finpos
    vrtnamelib.py
    - copied over from konversio repo

---
+++ tools/kielipankki/python/xvrt-tools/update.sh
@@ -0,0 +1,41 @@
+# bash this in xvrt-tools to copy files from another repo assuming you
+# have that other repo in place and up to date - - this is a temporary
+# setup till (relevant) vrt-tools are properly installed.
+
+other=../../../../../konversio/vrt-tools
+files=(
+    hrt-from-txt
+    hrtlib.py
+    hrt-tokenize-finnish-hfst
+    hrt-tokenize-udpipe
+    outsidelib.py
+    vrtargslib.py
+    vrtdatalib.py
+    vrt-finer
+    vrt-finpos
+    vrtnamelib.py
+    vrt-udpipe
+)
+
+for f in "${files[@]}"
+do
+    if test -f "$other/$f" -a -f "$f"
+    then
+	if cmp --quiet "$other/$f" "$f"
+	then
+	    echo ok "$f"
+	else
+	    echo updating "$f"
+	    cp "$other/$f" "$f"
+	fi
+    elif test -f "$other/$f"
+    then
+	echo adding "$f"
+	cp "$other/$f" "$f"
+    elif test -f "$f"
+    then
+	echo orphan "$f"
+    else
+	echo unknown "$f"
+    fi
+done
