f6f94b0e3f4e0b6e2f6ca4aacdbabdd762d03edd
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-04-18 17:16:59 +0300

    See if finnish-parse works even better with utf8ish() locale
    td-parse-ud1.py
    - in default environment took all manner of characters as ?
    - see if setting locale to some UTF-8 locale helps

--- tools/kielipankki/python/td-parse-ud1.py
+++ tools/kielipankki/python/td-parse-ud1.py
@@ -7,14 +7,21 @@ import os, sys
 from subprocess import Popen
 
 sys.path.append(os.path.join(chipster_module_path, "python"))
+sys.path.append(os.path.join(chipster_module_path, "python/xvrt-tools"))
 from lib_names2 import base, name
+
+from outsidelib import utf8ish
 
 name('output.txt', '{}-ud1'.format(base('input.txt', '*.txt')),
      ext = 'txt')
 
-# see if default finnish-parse works in Mylly 2019-04-18
+# see if default finnish-parse works in Mylly 2019-04-18;
+# it kind of seems to work but is full of question marks;
+# see if a utf8ish locale helps
 
 with Popen([ '/appl/ling/finnish-parse/1.0/bin/finnish-parse' ],
+           env = dict(os.environ,
+                      LC_ALL = utf8ish()),
            stdin  = open('input.txt', mode = 'rb'),
            stdout = open('output.txt', mode = 'wb')) as parse:
     pass
