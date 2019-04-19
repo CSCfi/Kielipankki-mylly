a4c41d3df0214718fac050cdabb59b7bba067d37
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-04-19 18:42:03 +0300

    Test if field name header can be in another file
    If this works, could extract a tiny TSV files from a VRT file to then
    provide field names for COLUMN_SEL for that VRT file! Maybe?
    
    ../kielipankki-module.xml
    test-meta.py
    - first input file should be vrt.txt
    - second should be .tsv
    - parameters should be filled from .tsv?
    
    Or maybe one of those should be marked as "meta"? If, which?

---
+++ tools/kielipankki/python/test-meta.py
@@ -0,0 +1,11 @@
+# TOOL test-meta.py: "Test if can have name file" (Test if field-name parameters can be filled in from another file - actual data in vrt.txt and names in a tsv)
+# INPUT data.vrt TYPE GENERIC
+# INPUT meta.tsv TYPE GENERIC
+# OUTPUT out.txt
+# PARAMETER name1: "first name" TYPE COLUMN_SEL
+# PARAMETER name2: "second name" TYPE COLUMN_SEL
+# RUNTIME python3
+
+with open('out.txt', 'w', encoding = 'UTF-8') as out:
+    print('name1 ==', name1, file = out)
+    print('name2 ==', name2, file = out)
