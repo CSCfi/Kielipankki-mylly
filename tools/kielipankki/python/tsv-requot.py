9bb1ddf6a287f1db90f52cfb507fae409017ff30
Jussi Piitulainen <jussi.piitulainen@helsinki.fi>
2019-04-19 12:12:16 +0300

    Add tool to replace quotes that confuse spreadsheet viewer
    Chipster spreadsheet viewer interprets, harmfully, at least the double
    quote, probably also apostrophe aka single quote.
    
    ../kielipankki-module.xml
    - add tsv-requot.py in TSV category
    
    tsv-requot.py
    - replaces ASCII "quotes" with Unicode right quotes

---
+++ tools/kielipankki/python/tsv-requot.py
@@ -0,0 +1,34 @@
+# TOOL tsv-requot.py: "Replace ASCII quotes" (Replace all ASCII quotes with right quotes.)
+# INPUT data.tsv TYPE GENERIC
+# OUTPUT result.tsv
+# RUNTIME python3
+
+import os, sys
+
+sys.path.append(os.path.join(chipster_module_path, "python"))
+from lib_names2 import base, name
+
+name('result.tsv', base('data.tsv', '*.tsv'),
+     ins = 'requot',
+     ext = 'tsv')
+
+reps = str.maketrans(( '\N{QUOTATION MARK}'
+                       '\N{APOSTROPHE}' )
+                     ,
+                     ( '\N{RIGHT DOUBLE QUOTATION MARK}'
+                       '\N{RIGHT SINGLE QUOTATION MARK}'
+                     ))
+
+try:
+    with open('data.tsv', encoding = 'UTF-8') as data:
+        head = next(data).rstrip('\n').split('\t')
+        with open('result.tsv', mode = 'w', encoding = 'UTF-8') as out:
+            print(number, *head, sep = '\t', file = out)
+            for k, line in enumerate(data, start = 1):
+                print(k, line.translate(reps),
+                      sep = '\t',
+                      end = '',
+                      file = out)
+except Exception as exn:
+    print(exn, file = sys.stderr)
+    exit(1)
